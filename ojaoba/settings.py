104122fb683b1aad8e3e8b58b0915748185b3bc9
Arise Damilare <arisedamilare5@gmail.com>
2026-01-19 12:30:33 +0100

    humanize

--- ojaoba/settings.py
+++ ojaoba/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "productApp",
+    "django.contrib.humanize"
 ]
 
 # client <= md => Server <= md => Database
