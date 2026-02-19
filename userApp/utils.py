c8766cc1a1a6742f5e4a70d44c15ec9da0350790
Arise Damilare <arisedamilare5@gmail.com>
2026-02-19 20:57:40 +0100

    Refactor utils

--- userApp/utils.py
+++ userApp/utils.py
@@ -1,37 +1,67 @@
 
 import requests
+from decouple import config
 
 def getCountries():
-    url = "https://country-state-city-search-rest-api.p.rapidapi.com/allcountries"
+    try:
+        url = "https://country-state-city-search-rest-api.p.rapidapi.com/allcountries"
 
-    headers = {
-        "x-rapidapi-key": "d5c5fe5c5dmshf006ed9f1ab63d6p1c8a6djsn1caa7867a7e8",
-        "x-rapidapi-host": "country-state-city-search-rest-api.p.rapidapi.com"
-    }
+        headers = {
+            "x-rapidapi-key": config("x-rapidapi-key"),
+            "x-rapidapi-host": config("x-rapidapi-host")
+        }
 
-    response = requests.get(url, headers=headers)
+        response = requests.get(url, headers=headers)
 
-    countries =[(country['isoCode'], country['name']) for country in response.json()]
-    
-    return countries
+        countries =[(country['isoCode'], country['name']) for country in response.json()]
+        
+        return countries
+    except Exception as e:
+        print(e)
+        return [
+            ('us', 'United States'),
+            ('ca', 'Canada'),
+            ('gb', 'United Kingdom'),
+            ('au', 'Australia'),
+            ('fr', 'France'),
+            ('de', 'Germany'),
+            ('jp', 'Japan'),
+            ('cn', 'China'),
+            ('in', 'India'),
+            ('br', 'Brazil')
+        ]
 
 
 def getState(isoCode):
-    
-    url = "https://country-state-city-search-rest-api.p.rapidapi.com/states-by-countrycode"
+    try:
+        url = "https://country-state-city-search-rest-api.p.rapidapi.com/states-by-countrycode"
 
-    querystring = {"countrycode":"us"}
+        querystring = {"countrycode":isoCode}
 
-    headers = {
-        "x-rapidapi-key": "d5c5fe5c5dmshf006ed9f1ab63d6p1c8a6djsn1caa7867a7e8",
-        "x-rapidapi-host": "country-state-city-search-rest-api.p.rapidapi.com"
-    }
+        headers = {
+            "x-rapidapi-key": config("x-rapidapi-key"),
+            "x-rapidapi-host": config("x-rapidapi-host")
+        }
 
-    response = requests.get(url, headers=headers, params=querystring)
-    
-    states = [(state['name'].lower(), state['name'] )for state in response.json()]
-    
-    return states
+        response = requests.get(url, headers=headers, params=querystring)
+        
+        states = [(state['name'].lower(), state['name'] )for state in response.json()]
+        
+        return states
+    except Exception as e:
+        print(e)
+        return [
+            ('california', 'California'),
+            ('texas', 'Texas'),
+            ('florida', 'Florida'),
+            ('new york', 'New York'),
+            ('illinois', 'Illinois'),
+            ('pennsylvania', 'Pennsylvania'),
+            ('ohio', 'Ohio'),
+            ('georgia', 'Georgia'),
+            ('north carolina', 'North Carolina'),
+            ('michigan', 'Michigan')
+        ]
 
 # print(getCountries())
 
